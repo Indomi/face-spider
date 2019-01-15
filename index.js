e64428ad3e56ff4444c45154cb4537f11306b5e9
shenyy <shenyy@corp.21cn.com>
2019-01-15 08:37:16 +0800

    'fix'

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getImgUrlList(pageNum) {
             imgArr.map((item, idx) => {
                 return download(item).then(data => {
                     let temp = item.split('/')
-                    let name = temp[temp.length - 1]
+                    let name = temp[temp.length - 1] + Date.parse(new Date())
                     setTimeout(function() {
                         fs.writeFile('dist/'+name, data, {
                             encoding: 'binary'
