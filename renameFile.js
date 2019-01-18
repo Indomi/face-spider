57a8b6d36693e070897e2858a6ecfef66cd0f1fd
shenyy <shenyy@corp.21cn.com>
2019-01-18 15:52:26 +0800

    '修复相对地址'

--- renameFile.js
+++ renameFile.js
@@ -5,7 +5,7 @@ let count = 0;
 fileList.map(item => {
     let lowerName = item.toLowerCase();
     if(!(lowerName.endsWith('.jpg') || lowerName.endsWith('.png') || lowerName.endsWith('.jpeg'))) {
-        fs.renameSync(item, item.split('.')[0] + '.jpg');
+        fs.renameSync('./assets/'+item, './assets/'+item.split('.')[0] + '.jpg');
         count++;
         console.log(`完成${count}个文件重命名`);
     }
